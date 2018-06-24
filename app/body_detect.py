4eb0ea70de671ee42e17bb910d65601425c64f3a
Umesh Senthilnathan <umesh.senthilnathan@gmail.com>
2018-06-24 16:48:33 -0400

    Uses backgroundSubtractor to draw contours

--- app/body_detect.py
+++ app/body_detect.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import sys
 
 def denoise(frame):
     frame = cv2.medianBlur(frame,5)
@@ -13,73 +14,100 @@ def facedetect(frame):
         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
 	return frame[y-30 :y+h + 30, x-30: x+h + 30]
 
+
 def nothing(x):
     pass
 
+def ObjectTract(frame):
+    tracker_types = ['BOOSTING', 'MIL','KCF', 'TLD', 'MEDIANFLOW', 'GOTURN']
+    tracker_type = tracker_types[2]
+
+    if tracker_type == 'BOOSTING':
+        tracker = cv2.TrackerBoosting_create()
+    if tracker_type == 'MIL':
+        tracker = cv2.TrackerMIL_create()
+    if tracker_type == 'KCF':
+        tracker = cv2.TrackerKCF_create()
+    if tracker_type == 'TLD':
+        tracker = cv2.TrackerTLD_create()
+    if tracker_type == 'MEDIANFLOW':
+        tracker = cv2.TrackerMedianFlow_create()
+    if tracker_type == 'GOTURN':
+        tracker = cv2.TrackerGOTURN_create()
+
+    bbox = cv2.selectROI("tracking", frame)
+
+    k = tracker.init(frame, bbox)
+
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        if not ret:
+            break
+        ok, bbox = tracker.update(frame)
+        
+        if ok:
+                # Tracking success
+            p1 = (int(bbox[0]), int(bbox[1]))
+            p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+            cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
+        else :
+            # Tracking failure
+            cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
+
+        # Display tracker type on frame
+        cv2.putText(frame, tracker_type + " Tracker", (100,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50),2);
+     
+        # Display the resulting frame
+        cv2.imshow('frame',frame)
+        #cv2.imshow('Erode',erodemask)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+cv2.namedWindow('PeriApprox')
+cv2.createTrackbar('Delta','PeriApprox',0,100,nothing)
+
 cap = cv2.VideoCapture(0)
+if not cap.isOpened(): 
+    print("Could not open the video camera")
+    sys.exit()
+
+ret, frame = cap.read() 
+if not ret:
+    print("Cannot read frame")
+    sys.exit()
+
 face_cascade = cv2.CascadeClassifier('/home/bobumble/Documents/scouter_app/app/haarcascade_frontalface_default.xml')
-cv2.namedWindow('BinaryThreshold')
-cv2.namedWindow('Erosion/Dilation')
-
-
-#TrackBar for the Filtering
-cv2.createTrackbar('minH','BinaryThreshold',0,255,nothing)
-cv2.createTrackbar('minS','BinaryThreshold',0,255,nothing)
-cv2.createTrackbar('minV','BinaryThreshold',0,255,nothing)
-cv2.createTrackbar('maxH','BinaryThreshold',255,255,nothing)
-cv2.createTrackbar('maxS','BinaryThreshold',255,255,nothing)
-cv2.createTrackbar('maxV','BinaryThreshold',255,255,nothing)
-
-#Trackbar for the erosion/dilation
-cv2.createTrackbar('Erode','Erosion/Dilation',0,10,nothing)
-cv2.createTrackbar('Dilate','Erosion/Dilation',0,10,nothing)
-cv2.createTrackbar('Kernel','Erosion/Dilation',1,10,nothing)
+fgbg = cv2.createBackgroundSubtractorMOG2()
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
+    if not ret:
+        break
+
+     # Define an initial bounding box
     face = facedetect(frame)
-    # Our operations on the frame come here
-    hsv = cv2.cvtColor(face, cv2.COLOR_BGR2HSV)
+    fgmask = fgbg.apply(frame)
 
-    minH = cv2.getTrackbarPos('minH','BinaryThreshold')
-    maxH = cv2.getTrackbarPos('maxH','BinaryThreshold')
-    minS = cv2.getTrackbarPos('minS','BinaryThreshold')
-    maxS = cv2.getTrackbarPos('maxS','BinaryThreshold')
-    minV = cv2.getTrackbarPos('minV','BinaryThreshold')
-    maxV = cv2.getTrackbarPos('maxV','BinaryThreshold')
+    image, contours, _ = cv2.findContours(fgmask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key = cv2.contourArea, reverse = True)[:3]
+    screenCnt = None
+    for c in contours :
+        peri = cv2.arcLength(c, True)
+        delta = cv2.getTrackbarPos('Delta','PeriApprox')/100
+        approx = cv2.approxPolyDP(c, delta * peri, True)
 
-    # define range of blue color in HSV
-    lower_bound = np.array([minH,minS,minV])
-    upper_bound = np.array([maxH,maxS,maxV])
-
-    # Threshold the HSV image to get only blue colors
-    mask = cv2.inRange(hsv, lower_bound, upper_bound)
-    
-    erodemask = mask
-    #applying erosion and dilation
-    erode = cv2.getTrackbarPos('Erode','Erosion/Dilation')
-    dilate = cv2.getTrackbarPos('Dilate','Erosion/Dilation')
-    element = cv2.getTrackbarPos('Kernel','Erosion/Dilation')
-    kernel = np.ones((element,element), np.uint8)
-    erodemask = cv2.erode(erodemask,  kernel, iterations=erode)
-    erodemask = cv2.dilate(erodemask,  kernel, iterations=dilate)
-
-    # Bitwise-AND mask and original image
-    #res = cv2.bitwise_and(frame,frame, mask= mask)
-
-    """
-    blur = cv2.GaussianBlur(hsv,(5,5),0)
-    ret3,th3 = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-    """
+        cv2.drawContours(frame, [approx], -1, (0, 255, 0), 3)
 
     # Display the resulting frame
-    cv2.imshow('hsv',hsv)
     cv2.imshow('frame',frame)
-    cv2.imshow('bg',mask)
-    cv2.imshow('Erode',erodemask)
+    cv2.imshow('fgmask', fgmask)
+
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+            break
+    
 
 # When everything done, release the capture
 cap.release()
